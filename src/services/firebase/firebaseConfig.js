baa685f45a0667c6d68baa848fc57d8160147494
bastianSalinas <bast.s.rojas@gmail.com>
2023-07-29 21:20:40 -0400

    se ocultan credenciales de firebase

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD52Xg5a53HvPWeLTsqQPVGwTHOBzBpdgw",
-  authDomain: "proyectoformorehp.firebaseapp.com",
-  projectId: "proyectoformorehp",
-  storageBucket: "proyectoformorehp.appspot.com",
-  messagingSenderId: "438453943385",
-  appId: "1:438453943385:web:9d7bd08b0a247c268fc548"
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
